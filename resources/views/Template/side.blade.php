6e2b86ab628b4873a730c76aada883087f5f79f3
josuafanter <129240984+josuafanter@users.noreply.github.com>
2023-07-10 13:33:45 +0700

    KP

--- resources/views/Template/side.blade.php
+++ resources/views/Template/side.blade.php
@@ -2,7 +2,7 @@
     <!-- Brand Logo -->
     <a class="brand-link">
       <img src="/img/logo.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
-      <span class="brand-text font-weight-light">The <strong>Project</strong></span>
+      <span class="brand-text font-weight-light">PLN <strong>Icon+</strong></span>
     </a>
 
     <!-- Sidebar -->
